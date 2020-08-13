25a449e0f51914ec8d96077565609b9943120450
Jack Mason <jack.mason1@ibm.com>
2020-08-13 12:01:07 +0100

    comments

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # Deno Weather CLI
 
+basic app to learn deno, returns weather in console.
+
 ## run
 
 `deno run --allow-read --allow-net index.ts --city London`
+
+*Output:*
+
+```
+.---------------------------------------------.
+|             Portsmouth Forecast             |
+|---------------------------------------------|
+|     Time ⏰     | Temp 🌡 |   Weather 🌤   |
+|-----------------|---------|-----------------|
+| 13th Aug, 13:00 | 23.5C   | light rain      |
+| 13th Aug, 16:00 | 23.0C   | light rain      |
+| 13th Aug, 19:00 | 22.0C   | light rain      |
+| 13th Aug, 22:00 | 19.7C   | light rain      |
+| 14th Aug, 1:00  | 19.2C   | moderate rain   |
+| 14th Aug, 4:00  | 18.9C   | light rain      |
+| 14th Aug, 7:00  | 18.9C   | light rain      |
+| 14th Aug, 10:00 | 20.6C   | broken clouds   |
+| 14th Aug, 13:00 | 20.9C   | broken clouds   |
+| 14th Aug, 16:00 | 22.0C   | overcast clouds |
+'---------------------------------------------'
+```
--- index.ts
+++ index.ts
@@ -8,40 +8,49 @@ import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 const args = parse(Deno.args);
 
+// Handle no city arg
 if (args.city === undefined) {
     console.error("No city supplied ❌");
     Deno.exit();
 }
 
+// get api key from .env 
 const apiKey = config()['API_KEY'];
 
+// fetch weather data from API
 const res = await fetch(
   `https://api.openweathermap.org/data/2.5/forecast?q=${args.city}&units=metric&appid=${apiKey}`,
 );
 const data = await res.json();
+console.log(data);
 
+// handle api errors
 if (data.cod !== "200") {
     console.error("API call failed ❌");
     console.error(data.message);
     Deno.exit();
 }
 
+// define typescript type for forecast item
 interface forecastItem {
   dt: string;
   main: { temp: number };
   weather: { description: string }[];
 }
 
-const forecast = data.list.slice(0, 8).map((item: forecastItem) => [
+// map each forecast into a forecast item
+const forecast = data.list.slice(0, 10).map((item: forecastItem) => [
   format(fromUnixTime(item.dt), "do LLL, k:mm", {}),
   `${item.main.temp.toFixed(1)}C`,
   item.weather[0].description,
 ]);
 
+// create ascii table from json data
 const table = AsciiTable.fromJSON({
   title: `${data.city.name} Forecast`,
   heading: ["Time ⏰", "Temp 🌡", "Weather 🌤"],
   rows: forecast,
 });
 
+// output the table
 console.log(table.toString());
